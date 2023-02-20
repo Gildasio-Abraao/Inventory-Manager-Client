095396e4118449e9b19382a9a2020a7b9dc76381
Gildasio Abraao <gildasio.abraao3@gmail.com>
2023-02-20 12:47:40 -0300

    Fix: response from api requests

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -75,7 +75,7 @@ export default {
     store() {
       ApiController.store(this.productForm)
         .then(response => {
-          this.message(response);
+          this.message(response.data);
           this.$emit('refresh');
         })
         .catch(error => {
@@ -85,7 +85,7 @@ export default {
     update() {
       ApiController.update(this.productForm)
         .then(response => {
-          this.message(response);
+          this.message(response.data);
           this.$emit('refresh');
         })
         .catch(error => {
@@ -93,11 +93,11 @@ export default {
         });
     },
     message(data) {
-      const status = data.status === 200 ? 'success' : 'error';
+      const status = data.code === 'Ok' ? 'success' : 'error';
       this.$toast.add({
         severity: status,
-        summary: data.code || data.status,
-        detail: data.message || data.statusText,
+        summary: data.code,
+        detail: data.message,
         life: 3000
       });
     }
